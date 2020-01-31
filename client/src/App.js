93fd9b8a8a03319ed5b50296a9e17c46a597312e
nixdie <nixdie@yandex.ru>
2020-01-31 17:29:17 +0700

    started with react on frontend

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <i class="fa fa-ambulance" aria-hidden="true"></i>
+      <h1>APP</h1>
+    </Fragment>
+  );
+}
+
+export default App;
