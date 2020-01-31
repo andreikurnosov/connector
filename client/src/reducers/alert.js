f0bbdedaf90303940427300c97e4f6a1aaa492df
nixdie <nixdie@yandex.ru>
2020-02-01 03:03:06 +0700

    alert component action call

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,9 +1,10 @@
-const initialState = [];
 import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
+
+const initialState = [];
 
 export default function(state = initialState, action) {
   const { type, payload } = action;
-  
+
   switch (type) {
     case SET_ALERT:
       return [...state, payload];
