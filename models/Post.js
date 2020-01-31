340f15b44cc8e4ff20ba26c832698800b380f6bf
nixdie <nixdie@yandex.ru>
2020-01-31 13:48:06 +0700

    add & delete routes

--- models/Post.js
+++ models/Post.js
@@ -28,26 +28,24 @@ const PostSchema = new Schema({
     {
       user: {
         type: Schema.Types.ObjectId,
-        ref: 'users',
-
-        text: {
-          type: String,
-          required: true
-        },
-        name: {
-          type: String
-        },
-        avatar: {
-          type: String
-        },
-        date: {
-          type: Date,
-          default: Date.now
-        }
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
       }
     }
   ],
-
   date: {
     type: Date,
     default: Date.now
