9f233fe515aebf3affd18b60013c0489963e03ee
nixdie <nixdie@yandex.ru>
2020-02-04 10:17:09 +0700

    started on the profile

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import EditProfile from './components/profile-form/EditProfile';
 import AddExperince from './components/profile-form/AddExperince';
 import AddEducation from './components/profile-form/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 
@@ -44,6 +45,7 @@ function App() {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               <PrivateRoute exact path="/edit-profile" component={EditProfile} />
